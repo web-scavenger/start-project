8cb1415d5a1c8c9f202a4ccf0624aa40558fe5a7
Dmytro Naumchuk <dnaumchuk@playtika.com>
2019-08-06 11:50:44 +0300

    add new description to README and new file to libs

---
+++ constants/index.js
@@ -0,0 +1,7 @@
+export const IOS_LINK = 'https://itunes.apple.com/US/app/id447553564';
+export const ANDROID_LINK = 'https://play.google.com/store/apps/details?id=air.com.playtika.slotomania';
+export const PC_LINK = 'https://www.slotomania.com/';
+
+export const IOS = 'iOS';
+export const ANDROID = 'Android';
+export const PC = 'PC';
